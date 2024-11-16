3407f0854d70e32cf34d6a383b834f3393004be4
Vijay Muthu <103064138+VijayMuthu-07@users.noreply.github.com>
2024-11-16 17:25:35 +0530

    Add files via upload

---
+++ alarm-clock.py
@@ -0,0 +1,32 @@
+import pygame
+import datetime
+import time
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = "D:/VS CODE/alarm clock/my_music.mp3"
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        if current_time == alarm_time:
+            print("Wake UP 😫")
+            
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+        
+            is_running = False
+        time.sleep(1)
+
+def main():
+    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
+
+if __name__ == '__main__':
+    main()
